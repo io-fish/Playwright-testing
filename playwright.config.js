4987168c978a4f9e045a62ee34991e7b59d7eb22
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2023-10-15 12:46:32 +0300

    Setup reporting

--- playwright.config.js
+++ playwright.config.js
@@ -21,14 +21,16 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['html'], ['allure-playwright']],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    // baseURL: 'https://mysite.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -38,15 +40,15 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/01_post_static_data.spec.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@playwright/test');
+let APIurl = 'https://restful-booker.herokuapp.com'
+test('should be able to create a booking', async ({ request }) => {
+    const response = await request.post(APIurl + '/booking', {
+        data: {
+            "firstname": "Jim",
+            "lastname": "Brown",
+            "totalprice": 111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2023-06-01",
+                "checkout": "2023-061-15"
+            },
+            "additionalneeds": "Breakfast"
+        }
+    });
+    expect(response.status()).toBe(200);
+    expect(response.ok()).toBeTruthy();
+    const responseBody = await response.json();
+    expect(responseBody.booking).toHaveProperty("firstname", "Jim");
+    expect(responseBody.booking).toHaveProperty("lastname", "Brown");
+    expect(responseBody.booking).toHaveProperty("totalprice", 111);
+    expect(responseBody.booking).toHaveProperty("depositpaid", true);
+    expect(responseBody.booking).toHaveProperty("bookingdates.checkin", "2023-06-01");
+    expect(responseBody.booking).toHaveProperty("bookingdates.checkout");
+    expect(responseBody.booking).toHaveProperty("additionalneeds", "Breakfast");
+    expect(responseBody).toHaveProperty("bookingid", responseBody.bookingid);
+});
